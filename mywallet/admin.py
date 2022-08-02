e89710cc74efdd09b9ce99b1c5523c2f5da8eea8
Your Name <joyywamaitha@gmail.com>
2022-08-02 18:27:11 +0300

    first commit

---
+++ mywallet/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Customer,Wallet,Currency,Reward,Receipts,Transaction,ThirdParty,Card,Loan,Notifications
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("first_name","last_name","address",)
+    search_fields = ("first_name","last_name",)
+admin.site.register(Customer,CustomerAdmin)
+admin.site.register(Receipts)
+admin.site.register(Transaction)
+admin.site.register(Card)
+admin.site.register(ThirdParty)
+admin.site.register(Currency)
+admin.site.register(Wallet)
+admin.site.register(Reward)
+admin.site.register(Loan)
+admin.site.register(Notifications)
+
+
+
