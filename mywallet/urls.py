dcea6124653fcf7c899411aaadf4f0461271cc9b
Your Name <joyywamaitha@gmail.com>
2022-09-14 21:16:47 +0300

    changes

--- mywallet/urls.py
+++ mywallet/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import account, card, currency, loan, notification, receipt, register_customer, reward, thirdparty, transaction, wallet
+from .views import account, card, currency, list_Customer, list_account, list_card, list_currency, list_loan, list_notification, list_receipt, list_reward, list_thirdparty, list_transaction, list_wallet, loan, notification, receipt, register_customer, reward, thirdparty, transaction, wallet
 
 urlpatterns = [
  path("register/",register_customer,name="registration"), 
@@ -13,7 +13,19 @@ urlpatterns = [
  path("notification",notification,name="notification"),
  path("receipt",receipt,name="receipt"),
  path("loan",loan,name="loan"),
- path("reward",reward,name="reward")
+ path("reward",reward,name="reward"),
+ path("customers",list_Customer,name="customers_list"),
+ path("walletlist",list_wallet,name="walletlist"),
+ path("currencylist",list_currency,name="currencylist"),
+ path("accountlist",list_account,name="accountlist"),
+ path("transactionlist",list_transaction,name="transactionlist"),
+ path("cardlist",list_card,name="cardlist"),
+ path("thirdpartylist",list_thirdparty,name="thirdpartylist"),
+ path("notificationlist",list_notification,name="notificationlist"),
+ path("receiptlist",list_receipt,name="receiptlist"),
+ path("loanlist",list_loan,name="loanlist"),
+ path("rewardlist",list_reward,name="rewardlist"),
+
  
 ]
  
--- mywallet/views.py
+++ mywallet/views.py
@@ -1,51 +1,152 @@
 from django.shortcuts import render
 from .forms import AccountRegistrationForm, CardRegistrationForm, CurrencyRegistrationForm, CustomerRegistrationForm, LoanRegistrationForm, NotificationsRegistrationForm, ReceiptRegistationForm, RewardRegistrtionForm, ThirdpartyRegistrationForm, TransactionRegistrationForm, WalletRegistrationForm
+from .models import Account, Card, Currency, Customer, Loan, Notifications, Receipts, Reward, ThirdParty, Transaction, Wallet
 
 # Create your views here.
 
 def register_customer(request):
-    form=CustomerRegistrationForm()
+    if request.method=="POST":
+       form=CustomerRegistrationForm(request.POST)
+       if form.is_valid():
+          form.save()
+    else:
+        form=CurrencyRegistrationForm()
     return render(request,"register_customer.html",{"form":form})
 
+def list_Customer(request):
+    customers=Customer.objects.all()
+    return render(request,"customer_list.html",{"customers":customers}) 
+
 def wallet(request):
-    form=WalletRegistrationForm()
+    if request.method=="POST":
+       form=WalletRegistrationForm(request.POST)
+       if form.is_valid():
+          form.save()
+    else:
+        form=WalletRegistrationForm()   
     return render(request,"wallet.html",{"form":form})
 
+def list_wallet(request):
+    wallets=Wallet.objects.all()
+    return render(request,"wallet_list.html",{"wallets":wallets})    
+
 def currency(request):
-    form=CurrencyRegistrationForm()
+    if request.method=="POST":
+       form=CurrencyRegistrationForm(request.POST)
+       if form.is_valid():
+          form.save()
+    else:
+        form=CurrencyRegistrationForm()
     return render(request,"currency.html",{"form":form})  
 
+def list_currency(request):
+    currencys=Currency.objects.all()
+    return render(request,"currency_list.html",{"currencys":currencys})    
+
 def account(request):
-    form=AccountRegistrationForm()
+    if request.method=="POST":
+       form=AccountRegistrationForm(request.POST)
+       if form.is_valid():
+          form.save()
+    else:
+        form=AccountRegistrationForm()
     return render(request,"account.html",{"form":form}) 
 
+def list_account(request):
+    accounts=Account.objects.all()
+    return render(request,"account_list.html",{"accounts":accounts})    
+
 def transaction(request):
-    form=TransactionRegistrationForm()
+    if request.method=="POST":
+       form=TransactionRegistrationForm(request.POST)
+       if form.is_valid():
+          form.save()
+    else:
+        form=TransactionRegistrationForm()      
     return render(request,"transaction.html",{"form":form}) 
 
+def list_transaction(request):
+    transactions=Transaction.objects.all() 
+    return render(request,"transaction_list.html",{"transactions":transactions})   
+
 def card(request):
-    form=CardRegistrationForm()
+    if request.method=="POST":
+       form=CardRegistrationForm(request.POST)
+       if form.is_valid():
+          form.save()
+    else:
+        form=CardRegistrationForm()      
     return render(request,"card.html",{"form":form})
 
+def list_card(request):
+    cards=Card.objects.all()
+    return render(request,"card_list.html",{"cards":cards})  
+
 def thirdparty(request):
-    form=ThirdpartyRegistrationForm()
+    if request.method=="POST":
+       form=ThirdpartyRegistrationForm(request.POST)
+       if form.is_valid():
+          form.save()
+    else:
+        form=ThirdpartyRegistrationForm()      
     return render(request,"thirdparty.html",{"form":form})
 
+def list_thirdparty(request):
+    partys=ThirdParty.objects.all()
+    return render(request,"thirdparty_list.html",{"partys":partys})    
+
 def notification(request):
-    form=NotificationsRegistrationForm()
+    if request.method=="POST":
+       form=NotificationsRegistrationForm(request.POST)
+       if form.is_valid():
+          form.save()
+    else:
+        form=NotificationsRegistrationForm()      
     return render(request,"notifications.html",{"form":form}) 
 
+def list_notification(request):
+    notifications=Notifications.objects.all()
+    return render(request,"notifications_list.html",{"notifications":notifications})  
+
 def receipt(request):
-    form=ReceiptRegistationForm()
+    if request.method=="POST":
+       form=ReceiptRegistationForm(request.POST)
+       if form.is_valid():
+          form.save()
+    else:
+        form=ReceiptRegistationForm()      
     return render(request,"receipt.html",{"form":form})
 
+def list_receipt(request):
+    receipts=Receipts.objects.all()
+    return render(request,"receipt_list.html",{"receipts":receipts})
+
+
 def loan(request):
-    form=LoanRegistrationForm()
+    if request.method=="POST":
+       form=LoanRegistrationForm(request.POST)
+       if form.is_valid():
+          form.save()
+    else:
+        form=LoanRegistrationForm()      
     return render(request,"loan.html",{"form":form}) 
 
+def list_loan(request):
+    loans=Loan.objects.all()
+    return render(request,"loan_list.html",{"loans":loans})   
+
 def reward(request):
-    form=RewardRegistrtionForm()
+    if request.method=="POST":
+        form=RewardRegistrtionForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form=RewardRegistrtionForm()
     return render(request,"reward.html",{"form":form})
+
+def list_reward(request):
+    rewards=Reward.objects.all()
+    return render(request,"reward_list.html",{"rewards":rewards})    
 
 
 
